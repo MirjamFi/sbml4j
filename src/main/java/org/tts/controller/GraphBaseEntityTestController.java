9964fe8fd3e22b195bb48de1533fc7e9f300d8fe
Thorsten Tiede <tiede@informatik.uni-tuebingen.de>
2019-09-05 16:31:37 +0200

    Test for sboChildren and compound-Resource-Type

--- src/main/java/org/tts/controller/GraphBaseEntityTestController.java
+++ src/main/java/org/tts/controller/GraphBaseEntityTestController.java
@@ -1,26 +1,51 @@
 package org.tts.controller;
 
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.tts.model.common.ExternalResourceEntity;
 import org.tts.model.common.GraphBaseEntity;
 import org.tts.model.common.SBMLSBaseEntity;
 import org.tts.service.GraphBaseEntityService;
+import org.tts.service.HttpService;
+import org.tts.service.SBMLService;
+import org.tts.service.SBMLSimpleModelUtilityServiceImpl;
+import org.tts.service.UtilityService;
 
 @RestController
 public class GraphBaseEntityTestController {
 
 	GraphBaseEntityService graphBaseEntityService;
+	
+	UtilityService utilityService;
+	
+	HttpService httpService;
+	
+	SBMLService sbmlService;
+	
+	SBMLSimpleModelUtilityServiceImpl sbmlSimpleModelUtilityServiceImpl;
 
 	@Autowired
-	public GraphBaseEntityTestController(GraphBaseEntityService graphBaseEntityService) {
+	public GraphBaseEntityTestController(	GraphBaseEntityService graphBaseEntityService,
+											UtilityService utilityService,
+											HttpService httpService,
+											@Qualifier("SBMLSimpleModelServiceImpl") SBMLService sbmlService,
+											SBMLSimpleModelUtilityServiceImpl sbmlSimpleModelUtilityServiceImpl) {
 		super();
 		this.graphBaseEntityService = graphBaseEntityService;
+		this.utilityService = utilityService;
+		this.httpService = httpService;
+		this.sbmlService = sbmlService;
+		this.sbmlSimpleModelUtilityServiceImpl = sbmlSimpleModelUtilityServiceImpl;
 	}
 	/**
 	 * Test GraphBaseEntity
@@ -67,7 +92,32 @@ public class GraphBaseEntityTestController {
 	}
 	
 	
+	@RequestMapping(value = "/testProc/sboChildren", method=RequestMethod.GET)
+	public ResponseEntity<Set<String>> getSBOChildren (@RequestParam(value = "parent") String parent,
+										@RequestParam(value = "mode") String mode) {
+		if(mode.equals("direct")) {
+			return new ResponseEntity<>(this.utilityService.getSBOChildren(parent), HttpStatus.OK);
+		} else if (mode.equals("full")) {
+			return new ResponseEntity<>(this.utilityService.getAllSBOChildren(parent), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+	}
 	
+	@RequestMapping(value = "/testProc/compound", method=RequestMethod.POST)
+	public ResponseEntity<ExternalResourceEntity> parseCompound(@RequestParam(value = "resource") String resource) {
+		if(!resource.contains("kegg.compound")) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} else {
+			ExternalResourceEntity testEntity = new ExternalResourceEntity();
+			this.sbmlSimpleModelUtilityServiceImpl.setGraphBaseEntityProperties(testEntity);
+			this.httpService.setCompoundAnnotationFromResource(resource, testEntity);
+			
+			
+			return new ResponseEntity<>(testEntity, HttpStatus.OK);
+		}
+	}
 	
 	
 	
